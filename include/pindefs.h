47b768c9f529dc24f42a4a2eb424c30eddd606b3
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-04-23 08:23:15 -0500

    Added pin definitions for TFT display module

--- include/pindefs.h
+++ include/pindefs.h
@@ -1,18 +1,21 @@
 #ifndef AADDE3B7_B737_4FCA_952B_F06FA0071CF7
 #define AADDE3B7_B737_4FCA_952B_F06FA0071CF7
 
-#define OLED_SDA 4
 #define BME_CS 2
+#define TFT_RST 3
+#define OLED_SDA 4
+#define TFT_DC 5
 #define DOWNBUTTON 12
 #define SELECTBUTTON 13
 #define RIGHTBUTTON 14
 #define OLED_SCL 15
 #define OLED_RST 16
-#define GPS_TX 22
 #define GPS_RX 17
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
+#define GPS_TX 22
+#define SCK 18
+#define MISO 19
+#define TFT_CS 21
+#define MOSI 23
 #define UV1 26
 #define UV2 27
 #define UV3 38
