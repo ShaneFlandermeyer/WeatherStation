5e04087ac7acd70bcb252b379a1d62df1f79f903
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-04-24 09:48:15 -0500

    Implemented TFT for main driver

--- src/WeatherStation.cpp
+++ src/WeatherStation.cpp
@@ -1,230 +1,138 @@
-// // #include <Adafruit_BME280.h>
-// // #include <Adafruit_Sensor.h>
-// #include <Arduino.h>
-// #include <HardwareSerial.h>
-// // #include <SPI.h>
-// // #include <TinyGPS++.h>
-// // #include <Wire.h>
-// #include <SD.h>
+// #include <Adafruit_BME280.h>
+// #include <Adafruit_Sensor.h>
+#include <Arduino.h>
+#include <HardwareSerial.h>
+// #include <SPI.h>
+// #include <TinyGPS++.h>
+// #include <Wire.h>
+#include <SD.h>
 
-// #include <sstream>
-// #include <vector>
+#include <sstream>
+#include <vector>
 
-// #include "bluetooth.h"
-// #include "display.h"
-// #include "file.h"
-// #include "pindefs.h"
-// #include "sensors.h"
+#include "bluetooth.h"
+#include "display.h"
+#include "file.h"
+#include "pindefs.h"
+#include "sensors.h"
 
-// // Configure GPS serial interface to use UART2
-// // HardwareSerial has to be used instead of Serial2 since the RX port for UART2
-// // also the reset pin for the OLED display (because heltec hates us >:( )
-// HardwareSerial SerialGPS(2);
+// Configure GPS serial interface to use UART2
+// HardwareSerial has to be used instead of Serial2 since the RX port for UART2
+// also the reset pin for the OLED display (because heltec hates us >:( )
+HardwareSerial SerialGPS(2);
+uint64_t dataTimer = millis();
 
-// /**
-//     Function declarations
-// */
-// void rightButtonPressed();
-// void leftbuttonPressed();
-// void selectButtonPressed();
-// void handleButtonPress();
 
-// /**
-//    OLED display variables
-// */
-// uint8_t frame = 0;
-// uint8_t line = 0;
-// uint8_t numFrames = 2;
-// uint8_t numSettings = 3;
-// uint32_t displayTimer = millis();
-// uint32_t dataTimer = millis();
-// uint32_t frameTimer = millis();
+// *******************************************************************************
+// * Setup functions
+//   *******************************************************************************
 
-// /*
-//     IO variables
-// */
-// volatile bool isRightButtonPressed = false;
-// volatile bool isLeftButtonPressed = false;
-// volatile bool isSelectButtonPressed = false;
-// volatile uint32_t buttonPressTime = 0;
+bool deviceConnected = false;
+class WeatherStationCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) { deviceConnected = true; };
+  void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
+};
 
-// // *******************************************************************************
-// // * Setup functions
-// //   *******************************************************************************
+void setup() {
+  // Input handling
+  pinMode(RIGHTBUTTON, INPUT_PULLUP);
+  pinMode(LEFTBUTTON, INPUT_PULLUP);
+  pinMode(SELECTBUTTON, INPUT_PULLUP);
+  // Attach interrupt functions
+  attachInterrupt(digitalPinToInterrupt(RIGHTBUTTON), rightButtonPressed,
+                  FALLING);
+  attachInterrupt(digitalPinToInterrupt(LEFTBUTTON), leftbuttonPressed,
+                  FALLING);
+  attachInterrupt(digitalPinToInterrupt(SELECTBUTTON), selectButtonPressed,
+                  FALLING);
+  Serial.begin(9600);
+  // Wait for USB Serial
+  while (not Serial) {
+    yield();
+  }
 
-// bool deviceConnected = false;
-// class WeatherStationCallbacks : public BLEServerCallbacks {
-//   void onConnect(BLEServer *pServer) { deviceConnected = true; };
-//   void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
-// };
+  // Start all devices
+  init_tft();
+  if (useOled) {
+    init_oled();
+  }
+  bme.begin();
+  BLEDevice::init("Portable Weather Station");
+  BLEServer *server = BLEDevice::createServer();
+  server->setCallbacks(new WeatherStationCallbacks());
+  BLEService *env_service = server->createService(ENV_SENSING_UUID);
+  BLEService *loc_service = server->createService(LOC_NAV_UUID);
 
-// void setup() {
-//   // Input handling
-//   pinMode(RIGHTBUTTON, INPUT_PULLUP);
-//   pinMode(LEFTBUTTON, INPUT_PULLUP);
-//   pinMode(SELECTBUTTON, INPUT_PULLUP);
-//   // Attach interrupt functions
-//   attachInterrupt(digitalPinToInterrupt(RIGHTBUTTON), rightButtonPressed,
-//                   FALLING);
-//   attachInterrupt(digitalPinToInterrupt(LEFTBUTTON), leftbuttonPressed,
-//                   FALLING);
-//   attachInterrupt(digitalPinToInterrupt(SELECTBUTTON), selectButtonPressed,
-//                   FALLING);
-//   Serial.begin(9600);
-//   // Wait for USB Serial
-//   while (not Serial) {
-//     yield();
-//   }
+  // Environmental sensing
+  env_service->addCharacteristic(&temperatureCharacteristic);
+  env_service->addCharacteristic(&humidityCharacteristic);
+  env_service->addCharacteristic(&pressureCharacteristic);
+  env_service->addCharacteristic(&windspeedCharacteristic);
+  env_service->addCharacteristic(&windDirectionCharacteristic);
 
-//   // Start all devices
-//   init_tft();
-//   if (useOled) {
-//     init_oled();
-//   }
-//   bme.begin();
-//   BLEDevice::init("Portable Weather Station");
-//   BLEServer *server = BLEDevice::createServer();
-//   server->setCallbacks(new WeatherStationCallbacks());
-//   BLEService *env_service = server->createService(ENV_SENSING_UUID);
-//   BLEService *loc_service = server->createService(LOC_NAV_UUID);
+  // Location and navigation
+  loc_service->addCharacteristic(&locationCharacteristic);
 
-//   // Environmental sensing
-//   env_service->addCharacteristic(&temperatureCharacteristic);
-//   env_service->addCharacteristic(&humidityCharacteristic);
-//   env_service->addCharacteristic(&pressureCharacteristic);
-//   env_service->addCharacteristic(&windspeedCharacteristic);
-//   env_service->addCharacteristic(&windDirectionCharacteristic);
+  // Start the service
+  env_service->start();
+  loc_service->start();
 
-//   // Location and navigation
-//   loc_service->addCharacteristic(&locationCharacteristic);
+  // Start advertising
+  BLEAdvertising *advertising = BLEDevice::getAdvertising();
+  advertising->addServiceUUID(ENV_SENSING_UUID);
+  advertising->addServiceUUID(LOC_NAV_UUID);
+  advertising->setScanResponse(true);
+  advertising->setMinPreferred(
+      0x06);  // functions that help with iPhone connections issue
+  advertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
 
-//   // Start the service
-//   env_service->start();
-//   loc_service->start();
+  SerialGPS.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
 
-//   // Start advertising
-//   BLEAdvertising *advertising = BLEDevice::getAdvertising();
-//   advertising->addServiceUUID(ENV_SENSING_UUID);
-//   advertising->addServiceUUID(LOC_NAV_UUID);
-//   advertising->setScanResponse(true);
-//   advertising->setMinPreferred(
-//       0x06);  // functions that help with iPhone connections issue
-//   advertising->setMinPreferred(0x12);
-//   BLEDevice::startAdvertising();
+  // Initialize the SD Card
+  if (not SD.begin()) {
+    Serial.println("No SD card attached");
+  }
+}
 
-//   SerialGPS.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
+// *******************************************************************************
+// * Processing loop
+// *******************************************************************************
+void loop() {
+  while (SerialGPS.available() > 0) {
+    char c = SerialGPS.read();
+    Serial.print(c);
+    gps.encode(c);
+  }
+  handleButtonPress();
 
-//   // Initialize the SD Card
-//   if (not SD.begin()) {
-//     Serial.println("No SD card attached");
-//   }
-// }
+  // Periodic data update
+  if (millis() - dataTimer >
+      settings.intervals[settings.dataUpdateIntervalIndex] * 1000) {
+    dataTimer = millis();
+    updateData(data, gps, settings.temperatureUnit);
+    writeData(SD, data, "/data.csv");
+    // Send the data over bluetooth low energy
+    if (deviceConnected) {
+      bleSendTemperature(data, settings);
+      bleSendHumidity(data);
+      bleSendPressure(data);
+      bleSendWindSpeed(data);
+      bleSendWindDirection(data);
+      bleSendGPSData(data);
+    }
+  }
 
-// // *******************************************************************************
-// // * Processing loop
-// // *******************************************************************************
-// void loop() {
-//   while (SerialGPS.available() > 0) {
-//     char c = SerialGPS.read();
-//     Serial.print(c);
-//     gps.encode(c);
-//   }
-//   handleButtonPress();
-
-//   // Periodic data update
-//   if (millis() - dataTimer >
-//       settings.intervals[settings.dataUpdateIntervalIndex] * 1000) {
-//     dataTimer = millis();
-//     updateData(data, gps, settings.temperatureUnit);
-//     writeData(SD, data, "/data.csv");
-//     // Send the data over bluetooth low energy
-//     if (deviceConnected) {
-//       bleSendTemperature(data, settings);
-//       bleSendHumidity(data);
-//       bleSendPressure(data);
-//       bleSendWindSpeed(data);
-//       bleSendWindDirection(data);
-//       bleSendGPSData(data);
-//     }
-//   }
-
-//   // Periodic display update
-//   if (millis() - displayTimer >
-//       settings.intervals[settings.displayUpdateIntervalIndex] * 1000) {
-//     displayTimer = millis();
-//     updateDisplay(oled, tft, data, settings, gps, frame, line);
-//   }
+  // Periodic display update
+  if (millis() - displayTimer >
+      settings.intervals[settings.displayUpdateIntervalIndex] * 1000) {
+    displayTimer = millis();
+    updateDisplay(oled, tft, data, settings, gps, tftState);
+  }
 
 //   if (millis() - frameTimer > 5000) {
 //     frameTimer = millis();
 //     frame = (frame + 1) % numFrames;
-//     updateDisplay(oled, tft, data, settings, gps, frame, line);
+//     updateDisplay(oled, tft, data, settings, gps, tftState);
 //   }
-// }
-
-// //****************************************************************************
-// // * Input handling and interrupts
-// //****************************************************************************
-
-// void handleButtonPress() {
-//   // Check if the user switched to a new menu tab
-//   if (isRightButtonPressed) {
-//     isRightButtonPressed = false;
-//     frame = (frame + 1) % numFrames;
-//     updateDisplay(oled, tft, data, settings, gps, frame, line);
-//   }
-
-//   // Check if the user switched to a new menu item
-//   if (isLeftButtonPressed) {
-//     isLeftButtonPressed = false;
-//     line = (line + 1) % numSettings;
-//     updateDisplay(oled, tft, data, settings, gps, frame, line);
-//   }
-
-//   // Check if the user clicked on an item
-//   if (isSelectButtonPressed) {
-//     isSelectButtonPressed = false;
-//     switch (line) {
-//       case DISPLAYRATE:  // Update rate changed
-//         settings.displayUpdateIntervalIndex =
-//             (settings.displayUpdateIntervalIndex + 1) %
-//             settings.intervals.size();
-//         break;
-//       case DATARATE:
-//         settings.dataUpdateIntervalIndex =
-//             (settings.dataUpdateIntervalIndex + 1) % settings.intervals.size();
-//         break;
-//       case TEMPERATUREUNIT:  // Temperature unit changed
-//         settings.temperatureUnit =
-//             (settings.temperatureUnit == FAHRENHEIT ? CELSIUS : FAHRENHEIT);
-//         break;
-//       default:
-//         break;
-//     }
-//     updateDisplay(oled, tft, data, settings, gps, frame, line);
-//   }
-// }
-
-// void rightButtonPressed() {
-//   int timeNow = millis();
-//   if (timeNow > buttonPressTime + 250) {
-//     isRightButtonPressed = true;
-//     buttonPressTime = timeNow;
-//   }
-// }
-
-// void leftbuttonPressed() {
-//   int timeNow = millis();
-//   if (timeNow > buttonPressTime + 250) {
-//     isLeftButtonPressed = true;
-//     buttonPressTime = timeNow;
-//   }
-// }
-
-// void selectButtonPressed() {
-//   int timeNow = millis();
-//   if (timeNow > buttonPressTime + 250) {
-//     isSelectButtonPressed = true;
-//     buttonPressTime = timeNow;
-//   }
-// }
+}
--- src/displayexample.cpp
+++ src/displayexample.cpp
@@ -1,68 +1,46 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-#include "display.h"
-#include "pindefs.h"
+// #include "display.h"
+// #include "pindefs.h"
 
-/**
-    Function declarations
-*/
-// void rightButtonPressed();
-// void leftbuttonPressed();
-// void selectButtonPressed();
-// void handleButtonPress();
+// void setup() {
+//   // Input handling
+//   pinMode(RIGHTBUTTON, INPUT_PULLUP);
+//   pinMode(LEFTBUTTON, INPUT_PULLUP);
+//   pinMode(SELECTBUTTON, INPUT_PULLUP);
+//   // Attach interrupt functions
+//   attachInterrupt(digitalPinToInterrupt(RIGHTBUTTON), rightButtonPressed,
+//                   FALLING);
+//   attachInterrupt(digitalPinToInterrupt(LEFTBUTTON), leftbuttonPressed,
+//                   FALLING);
+//   attachInterrupt(digitalPinToInterrupt(SELECTBUTTON), selectButtonPressed,
+//                   FALLING);
+//   Serial.begin(9600);
+//   // Wait for USB Serial
+//   while (not Serial) {
+//     yield();
+//   }
 
-/**
-   Display variables
-   TODO: Move to display.h
-   TODO: Add OLED variables
-*/
-// uint8_t line = 0;
-// uint8_t frame = 0;
+//   // Start all devices
+//   init_tft();
+//   if (useOled) {
+//     init_oled();
+//   }
+// }
 
-
-/*
-    IO variables
-*/
-// volatile uint32_t buttonPressTime = 0;
-
-void setup() {
-  // Input handling
-  pinMode(RIGHTBUTTON, INPUT_PULLUP);
-  pinMode(LEFTBUTTON, INPUT_PULLUP);
-  pinMode(SELECTBUTTON, INPUT_PULLUP);
-  // Attach interrupt functions
-  attachInterrupt(digitalPinToInterrupt(RIGHTBUTTON), rightButtonPressed,
-                  FALLING);
-  attachInterrupt(digitalPinToInterrupt(LEFTBUTTON), leftbuttonPressed,
-                  FALLING);
-  attachInterrupt(digitalPinToInterrupt(SELECTBUTTON), selectButtonPressed,
-                  FALLING);
-  Serial.begin(9600);
-  // Wait for USB Serial
-  while (not Serial) {
-    yield();
-  }
-
-  // Start all devices
-  init_tft();
-  if (useOled) {
-    init_oled();
-  }
-}
-
-// *******************************************************************************
-// * Processing loop
-// *******************************************************************************
-void loop() {
-  // Periodic display update
-  handleButtonPress();
-  if (millis() - displayTimer >
-      1000) {
-    displayTimer = millis();
-    updateDisplay(oled, tft, data, settings, gps, tftState);
-  }
-  data.temperature += 1;
-}
+// // *******************************************************************************
+// // * Processing loop
+// // *******************************************************************************
+// void loop() {
+//   // Periodic display update
+//   handleButtonPress();
+//   if (millis() - displayTimer >
+//       1000) {
+//     displayTimer = millis();
+//     updateDisplay(oled, tft, data, settings, gps, tftState);
+//   }
+//   data.temperature += 1;
+// }
 
 
 
